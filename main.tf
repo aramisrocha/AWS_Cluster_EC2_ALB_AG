547ed2e1a11030e3890b79a4e1940d89331b842c
Aramis de Oliveira Andrade Rocha <aramis.rocha@ativas.com.br>
2023-11-01 12:01:40 -0300

    Adicionando tabela de rotas

--- main.tf
+++ main.tf
@@ -6,9 +6,16 @@ terraform {
   }
 }
 
+
+
+
 provider "aws" {
   region = "${var.region}"
 }
+
+
+
+# Criando os recursos de rede
 
 
 resource "aws_vpc" "vpc_LAB" {
@@ -24,4 +31,55 @@ resource "aws_subnet" "Subnet_LAB" {
   vpc_id          = aws_vpc.vpc_LAB.id
   cidr_block      = cidrsubnet(var.network_cidr, 8, count.index)
   availability_zone = element(["us-east-1a", "us-east-1b"], count.index % 2)
+}
+
+
+# Adicionando um security group para acesso ao WEB
+
+resource "aws_security_group" "SG_WEB" {
+  name        = "Security group para os servidores WEB"
+  description = "Permitit somente acesso a WEB"
+  vpc_id      = aws_vpc.vpc_LAB.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  }
+
+
+  resource "aws_internet_gateway" "Gateway_LAB" {
+  vpc_id = aws_vpc.vpc_LAB.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc_LAB.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Gateway_LAB.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet" {
+  count = var.subnet_count
+  subnet_id      = aws_subnet.Subnet_LAB[count.index].id
+  route_table_id = aws_route_table.public.id
+  depends_on     = [aws_internet_gateway.Gateway_LAB]
 }
