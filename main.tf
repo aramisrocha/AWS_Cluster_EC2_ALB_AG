2568eb134bcefb1e6b3b024c88ef28398c4d4456
Aramis de Oliveira Andrade Rocha <aramis.rocha@ativas.com.br>
2023-11-02 19:55:52 -0300

    Terraform apply funcionou

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "Subnet_LAB" {
   count           = var.subnet_count
   vpc_id          = aws_vpc.vpc_LAB.id
   cidr_block      = cidrsubnet(var.network_cidr, 8, count.index)
-  availability_zone = element(["us-east-1a", "us-east-1b"], count.index % 2)
+  availability_zone = element(["us-east-2a", "us-east-2b"], count.index % 2)
 }
 
 
@@ -78,16 +78,38 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
+#Adicionando internet gateway
+  resource "aws_internet_gateway" "Gateway_LAB" {
+  vpc_id = aws_vpc.vpc_LAB.id
+}
+
+# Criando uma Tabela de rotas ja associando o internet gateway
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc_LAB.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Gateway_LAB.id
+  }
+}
+
+#Associando a tabela de rotas as duas subnets e definindo a criação do IG como depenencia
+resource "aws_route_table_association" "public_subnet" {
+  count = length(aws_subnet.Subnet_LAB)
+  subnet_id      = aws_subnet.Subnet_LAB[count.index].id
+  route_table_id = aws_route_table.public.id
+  depends_on     = [aws_internet_gateway.Gateway_LAB]
+}
 
 # Criando o template das maquinas 
 resource "aws_launch_configuration" "LAB01" {
   name_prefix          = "Template para o LAB01"
   image_id             = data.aws_ami.latest_amazon_linux.id
   instance_type        = var.instance_type
-  security_groups      = [aws_security_group.SG_WEB.name]
+  security_groups      = [aws_security_group.SG_WEB.id]
   key_name             = var.instance_key_name
   user_data            = filebase64("ec2_setup.sh")
-  iam_instance_profile = aws_iam_role.role_lab01.name
+  #iam_instance_profile = aws_iam_role.role_lab01.name
 }
 
 
@@ -104,33 +126,33 @@ resource "aws_lb_target_group" "TG_lab01" {
 
 
 
-resource "aws_autoscaling_group" "ASG_LAB01" {
-  name                      = "ASG LAB 01"
+resource "aws_autoscaling_group" "ASG_LAB001" {
+  name                      = "ASGLAB001"
   #count = 2
   launch_configuration       = aws_launch_configuration.LAB01.name
   min_size                  = 2
   max_size                  = 4
   desired_capacity          = 2
-  #vpc_zone_identifier       = [aws_subnet.Subnet_LAB[count.index].id]
+  vpc_zone_identifier       = [for subnet in aws_subnet.Subnet_LAB : subnet.id]
   health_check_type         = "EC2"
   health_check_grace_period = 300
-  availability_zones        = ["us-east-1a", "us-east-1b"]
+ #availability_zones        = ["us-east-2a", "us-east-2b"]
   target_group_arns         = [aws_lb_target_group.TG_lab01.arn]
 }
 
 
 # Crie um recurso de Application Load Balancer
-resource "aws_lb" "example" {
-  name               = "example-alb"
+resource "aws_lb" "ALB_lab01" {
+  name               = "ALB-lab01"
   internal           = false
   load_balancer_type = "application"
-  subnets            = ["subnet-1", "subnet-2"] # Substitua pelos IDs das subnets
+  subnets            = [for subnet in aws_subnet.Subnet_LAB : subnet.id]
   security_groups    = [aws_security_group.SG_WEB.id]
 }
 
 # Defina os listeners de encaminhamento do ALB (pode haver vários listeners)
 resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.example.arn
+  load_balancer_arn = aws_lb.ALB_lab01.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "latest_ami_id" {
   value = data.aws_ami.latest_amazon_linux.id
 }
+
+
+output "alb_dns_name" {
+  description = "DNS de acesso ao Load balance"
+  value       = aws_lb.ALB_lab01.dns_name
+}
